ad9bbfad25c7e41bc09357be4b010aa0c8e31723
ashwinGokhale <gokhale0@purdue.edu>
2018-02-11 21:25:57 -0500

    Migrating from Realtime Database to Firestore

---
+++ functions/src/routes/news.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+import * as firebase from 'firebase-admin'
+export const router = express.Router();
+
+router.get("/", async (req, res) => {
+	try {
+		const resp = await firebase.firestore().collection('/news').get();
+		res.json(resp.docs.map(doc => doc.data()))
+	} catch (err) {
+		res.sendStatus(500).send(err);
+	}
+});
