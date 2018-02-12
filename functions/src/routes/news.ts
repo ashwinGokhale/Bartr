5baa938dead8ee58e624dd6d7e465489c78a666b
ashwinGokhale <gokhale0@purdue.edu>
2018-02-11 21:21:25 -0500

    Added support for Algolia searching

--- functions/src/routes/news.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-import * as firebase from 'firebase'
-export const router = express.Router();
-
-router.get("/", (req, res) => {
-	firebase.database().ref('/news').once('value').then(resp => {
-	  res.json(resp.val())
-	});
-});
