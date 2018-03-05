b1c6b3e94a23b8bc039056a71111641e93315880
ashwinGokhale <gokhale0@purdue.edu>
2018-03-04 21:30:18 -0500

    Successfully querying posts from AccountPage

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session';
 import userReducer from './user';
 import settingsReducer from './settings';
+import postsReducer from './posts'
 
 export default combineReducers({
   sessionState: sessionReducer,
   userState: userReducer,
-  settingsState: settingsReducer
+  settingsState: settingsReducer,
+  postsState: postsReducer
 });
