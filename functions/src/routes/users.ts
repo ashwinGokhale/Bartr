4b4616a1f96179a298fe1077fb31dd4829d9bf1d
ashwinGokhale <gokhale0@purdue.edu>
2018-03-11 01:19:56 -0500

    Destructured _geoloc in users post

--- functions/src/routes/users.ts
+++ functions/src/routes/users.ts
@@ -58,7 +58,8 @@ router.post('/:uid', async (req, res) => {
 								email: req.body.email,
 								phoneNumber: req.body.phoneNumber || ''
 							},
-							_geoloc: req.body._geoloc || { lat: 0, lng: 0 },
+							lat: 0, 
+							lng: 0 ,
 							radius: 5
 						})
 						res.status(200).send(`User: ${req.params.uid} created at: ${newUser.writeTime}`)
@@ -74,54 +75,6 @@ router.post('/:uid', async (req, res) => {
 	}
 	else 
 		res.status(401).send('Unauthorized')
-
-
-
-	// firebase.firestore().doc(`/users/${req.params.uid}`).get()
-	// 	.then(data => {
-	// 		// Create new user
-	// 		if (!data.exists) {
-	// 			if (!req.params.uid) res.status(400).send('User must have a uid')
-	// 			else if (!req.body.photoUrl) res.status(400).send('User must have a photo url')
-	// 			else if (!req.body.email) res.status(400).send('User must have a description')
-	// 			else if (!req.body.displayName) res.status(400).send('User must have a display name')
-	// 			else {
-	// 				data.ref.set({
-	// 					uid: req.params.uid,
-	// 					photoUrl: req.body.photoUrl,
-	// 					displayName: req.body.displayName,
-	// 					contactInfo: {
-	// 						address: req.body.address || '',
-	// 						email: req.body.email,
-	// 						phoneNumber: req.body.phoneNumber || ''
-	// 					},
-	// 					_geoloc: req.body._geoloc || { lat: 0, lng: 0 },
-	// 					radius: 5
-	// 				})
-	// 				.then(newUser => res.status(200).send(`User: ${req.params.uid} created at: ${newUser.writeTime}`))
-	// 				.catch(error => res.status(400).send(error))
-	// 			}
-	// 		}
-			
-	// 		// Update a user
-	// 		else {
-	// 			const originalUser = data.data()
-	// 			data.ref.set({
-	// 				photoUrl: req.body.photoUrl || originalUser.photoUrl,
-	// 				displayName: req.body.displayName || originalUser.displayName,
-	// 				contactInfo: {
-	// 					address: req.body.address || originalUser.contactInfo.address,
-	// 					email: req.body.email || originalUser.contactInfo.email,
-	// 					phoneNumber: req.body.phoneNumber || originalUser.contactInfo.phoneNumber
-	// 				},
-	// 				_geoloc: req.body._geoloc || originalUser._geoloc,
-	// 				radius: (req.body.radius && req.body.radius >= 5) ? req.body.radius : originalUser.radius
-	// 			}, { merge: true })
-	// 			.then(newUser => res.status(200).send(`User: ${req.params.uid} created at: ${newUser.writeTime}`))
-	// 			.catch(error => res.status(400).send(error))
-	// 		}
-	// 	})
-	// 	.catch(error => res.status(400).send(error))
 });
 
 router.put('/:uid', async (req, res) => {
